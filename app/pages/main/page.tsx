e7c8b9bd8551c283f29570d6e9ba8388749ca213
Rafa <rafafajari230907@gmail.com>
2024-12-23 14:37:47 +0700

    Add: Avatar mouth

--- app/pages/main/page.tsx
+++ app/pages/main/page.tsx
@@ -7,47 +7,45 @@ import BubbleComponent from "@/components/bubble-container/bubble";
 import WebSocketGreeting from '@/components/welcome/welcome';
 
 export default function MainPage() {
-  const { avatar } = useContext(AvatarContext);
-  // State to store the greeting
-  const [greeting, setGreeting] = useState("");
-  const [isSpeaking, setIsSpeaking] = useState(false);
+    const {avatar} = useContext(AvatarContext);
+    // State to store the greeting
+    const [greeting, setGreeting] = useState("");
+    const [isSpeaking, setIsSpeaking] = useState(false);
 
-useEffect(()=>{
-  if (greeting){
-    setIsSpeaking(true);
-    const timer = setTimeout(()=> setIsSpeaking(false),3000);
-    return () => clearTimeout(timer);
-  }
-},[greeting]);
-  return (
-    <div className={classes.body}>
-      <div className={classes.wrapper}>
-        <div className={classes.wrapperIcon}>
-          <IconSettings />
+    // useEffect(() => {
+    //     if (greeting) {
+    //         setIsSpeaking(true);
+    //         const timer = setTimeout(() => setIsSpeaking(false), 3000);
+    //         return () => clearTimeout(timer);
+    //     }
+    // }, [greeting]);
+
+    return (
+        <div className={classes.body}>
+            <div className={classes.wrapper}>
+                <div className={classes.wrapperIcon}>
+                    <IconSettings/>
+                </div>
+                <div className={classes.wrapperImg}>
+                    <BubbleComponent content={greeting}/>
+                    <div className={classes.imgContainer}>
+                        {isSpeaking ?
+                            <img className={classes.imgProfile} src={`/${avatar}.gif`}/> :
+                            <img
+                                className={classes.imgProfile}
+                                src={`/${avatar}.png`}
+                            />
+                        }
+                    </div>
+                </div>
+                <div className={classes.hidden}>
+                    <IconSettings/>
+                </div>
+                <WebSocketGreeting
+                    aigender={avatar}
+                    setGreetingCallback={setGreeting}
+                />
+            </div>
         </div>
-        <div className={classes.wrapperImg}>
-          <BubbleComponent content={greeting} />
-          <div className={classes.imgContainer}>
-            <img
-              src="/mouth.gif"
-              alt="Mouth Animation"
-              className={classes.gif}
-            />
-            <img
-              className={classes.imgProfile}
-              src={`/${avatar}.svg`}
-              alt="Selected Avatar"
-            />
-          </div>
-        </div>
-        <div className={classes.hidden}>
-          <IconSettings />
-        </div>
-        <WebSocketGreeting
-          aigender={avatar}
-          setGreetingCallback={setGreeting}
-        /> 
-      </div>
-    </div>
-  );
+    );
 }
